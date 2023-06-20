d214757e0ee97886dbb1492d75d8efe8f23845af
SerzhantovDimitry <serzhantovdimitri@yandex.ru>
2023-06-20 18:19:28 +0300

    dddd

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.post(
         password: Joi.string().required().min(2),
         name: Joi.string().min(2).max(30),
         about: Joi.string().min(2).max(30),
-        avatar: Joi.string().validate,
+        avatar: Joi.string(),
       })
       .unknown(true),
   }),
