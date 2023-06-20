20af16e1efdcbd52040edbfecef38a5dc10c2f9e
SerzhantovDimitry <serzhantovdimitri@yandex.ru>
2023-06-20 21:23:29 +0300

    dddd

--- routes/users.js
+++ routes/users.js
@@ -22,7 +22,18 @@ router.get(
   getUserById
 );
 
-router.patch('/users/me', updateUser);
+router.patch(
+  '/users/me',
+  celebrate({
+    body: Joi.object()
+      .keys({
+        name: Joi.string().min(2).max(30),
+        about: Joi.string().min(2).max(30),
+      })
+      .unknown(true),
+  }),
+  updateUser
+);
 
 router.patch('/users/me/avatar', updateUserAvatar);
 
