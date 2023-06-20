9c93ba8167d8beb7fa5d097c5e9432d011b12c3b
SerzhantovDimitry <serzhantovdimitri@yandex.ru>
2023-06-20 19:18:29 +0300

    dddd

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,20 +1,3 @@
-// eslint-disable-next-line max-classes-per-file
-class UserNotFound extends Error {
-  constructor(err) {
-    super(err);
-    this.message = 'пользователь не найден';
-    this.statusCode = 404;
-  }
-}
-
-class AbstractError extends Error {
-  constructor(err) {
-    super(err);
-    this.message = 'Внутренняя ошибка сервера!!!)';
-    this.statusCode = 500;
-  }
-}
-
 const errorHandler = (err, req, res, next) => {
   if (String(err.message) === 'error') {
     res.status(401).send({ message: 'Не верные данные пользователя!!!' });
