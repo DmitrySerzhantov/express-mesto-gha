7616a90ea6dd60d5fb302820c7fd6dbc4f31ccce
SerzhantovDimitry <serzhantovdimitri@yandex.ru>
2023-06-21 04:54:00 +0300

    2proverka

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,7 @@ const {
   updateUserAvatar,
   userProfile,
 } = require('../controllers/users');
+const { regularValidetUrl } = require('../utils/constants');
 
 router.get('/users', getUsers);
 router.get('/users/me', userProfile);
@@ -40,9 +41,7 @@ router.patch(
   celebrate({
     body: Joi.object()
       .keys({
-        avatar: Joi.string().pattern(
-          /^(?:(?:https?|HTTPS?|ftp|FTP):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-zA-Z\u00a1-\uffff0-9]-*)*[a-zA-Z\u00a1-\uffff0-9]+)(?:\.(?:[a-zA-Z\u00a1-\uffff0-9]-*)*[a-zA-Z\u00a1-\uffff0-9]+)*)(?::\d{2,})?(?:[/?#]\S*)?$/,
-        ),
+        avatar: Joi.string().pattern(regularValidetUrl),
       })
       .unknown(true),
   }),
