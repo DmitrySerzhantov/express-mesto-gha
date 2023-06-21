3d3316b6b249705074b74e8d7ca744926455bae0
SerzhantovDimitry <serzhantovdimitri@yandex.ru>
2023-06-21 04:46:01 +0300

    2proverka

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const router = require('./routes');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/error');
+const regularValidetUrl = require('./utils/constants');
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
@@ -31,9 +32,7 @@ app.post(
       password: Joi.string().required().min(2),
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string().pattern(
-        /^(?:(?:https?|HTTPS?|ftp|FTP):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-zA-Z\u00a1-\uffff0-9]-*)*[a-zA-Z\u00a1-\uffff0-9]+)(?:\.(?:[a-zA-Z\u00a1-\uffff0-9]-*)*[a-zA-Z\u00a1-\uffff0-9]+)*)(?::\d{2,})?(?:[/?#]\S*)?$/,
-      ),
+      avatar: Joi.string().pattern(regularValidetUrl),
     }),
   }),
   createUser,
