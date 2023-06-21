6806b5792bf3804ce9550844edb1066afce3ae81
SerzhantovDimitry <serzhantovdimitri@yandex.ru>
2023-06-21 16:48:38 +0300

    c day

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
+const NotFound = require('../errors/NotFoundError');
 
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 
 router.use(userRoutes);
 router.use(cardRoutes);
-router.use('*', (req, res) => {
-  res.status(404).send({ message: 'Страница не найдена' });
+router.use('*', (err, req, res, next) => {
+  next(err, new NotFound('Страница не найдена'));
 });
 
 module.exports = router;
