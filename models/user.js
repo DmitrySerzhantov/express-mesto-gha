5c885220167da4b76119b6efa06e17f7f1baf3eb
SerzhantovDimitry <serzhantovdimitri@yandex.ru>
2023-06-21 16:58:51 +0300

    c day

--- models/user.js
+++ models/user.js
@@ -15,13 +15,14 @@ const userSchema = new mongoose.Schema({
     default: 'Исследователь',
   },
   avatar: {
-    type: URL,
+    type: String,
     default:
       'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
     unique: true,
+    minlength: 2,
     required: [true, 'Поле email должно быть заполнено'],
     validate(value) {
       if (!validator.isEmail(value)) {
@@ -31,9 +32,16 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type: String,
+    minlength: 2,
     required: [true, 'Поле password должно быть заполнено'],
     select: false,
   },
 });
+
+userSchema.methods.toJSON = function deletePassword() {
+  const user = this.toObject();
+  delete user.password;
+  return user;
+};
 
 module.exports = mongoose.model('user', userSchema);
